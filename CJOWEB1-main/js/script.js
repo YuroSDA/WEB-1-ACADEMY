1629774f29eed7188d4075abcc302211107b3004
AlessandroRSousa <ribeiroalessandro503@gmail.com>
2026-06-18 00:45:43 -0300

    adc matricula, funçao(testar), imagem background não entrou, z-index adcionado, footer alterado.

--- CJOWEB1-main/js/script.js
+++ CJOWEB1-main/js/script.js
@@ -12,3 +12,29 @@ function mostrarDica() {
     const inputDica = document.getElementById("dica");
     inputDica.style.display = "block";
 }
+
+
+// Adiciona um evento de clique ao botão para mostrar a dica
+document.getElementById("mostrarDica").addEventListener("click", mostrarDica);
+
+//Função elemento 'range' estilo de treino
+let range = document.getElementById("range");
+let nivel = document.getElementById("nivel");
+
+function atualizarNivel() {
+    let valor = Number(range.value);
+
+    if (valor <= 3) {
+        nivel.textContent = valor + " = treino fofo 🥹";
+    } else if (valor <= 7) {
+        nivel.textContent = valor + " = saudável 😃";
+    } else {
+        nivel.textContent = valor + " = fortão 💪";
+    }
+}
+
+range.addEventListener("input", atualizarNivel);
+
+atualizarNivel();
+
+console.log("script carregaodo com sucesso!");
