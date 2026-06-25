098e92a6f32eafa2b5755028d1537412b9571051
Yuri Emanuel <143857432+YuroSDA@users.noreply.github.com>
2026-06-25 20:32:46 -0300

    Add files via upload

--- CJOWEB1-main/js/script.js
+++ CJOWEB1-main/js/script.js
@@ -11,25 +11,59 @@ function mostrarDica() {
     document.getElementById("dica").value = dicas[indice];
     const inputDica = document.getElementById("dica");
     inputDica.style.display = "block";
+    
 }
 
-//Função elemento 'range' estilo de treino
-let range = document.getElementById("range");
-let nivel = document.getElementById("nivel");
+// Função do estilo de treino
+
 
 function atualizarNivel() {
-    let valor = Number(range.value);
+    let nivel = parseInt(document.getElementById("nivel").value);
+    const inputNivel = document.getElementById("nivel");
+    inputNivel.style.display = "inline";
 
-    if (valor <= 3) {
-        nivel.textContent = valor + " = treino fofo 🥹";
-    } else if (valor <= 7) {
-        nivel.textContent = valor + " = saudável 😃";
+    if (nivel <= 3) {
+        alert("Deixa de Ser frango!!")
+        inputNivel.style.display = "none";
+    } else if (nivel <= 7) {
+        alert("Estou te achando muito fraco!!")
+        inputNivel.style.display = "none";
     } else {
-        nivel.textContent = valor + " = fortão 💪";
+        alert("Agora eu gostei!!!!")
+        inputNivel.style.display = "none";
     }
+    
 }
 
+function calcularIMC() {
+    let peso = parseFloat(document.getElementById("peso").value);
+    let altura = parseFloat(document.getElementById("altura").value);
 
-atualizarNivel();
+    if (isNaN(peso) || isNaN(altura) || peso <= 0 || altura <= 0) {
+        document.getElementById("resultado").innerHTML =
+            "Digite valores válidos!";
+        return;
+    }
+    //Calculadora de IMC
+    let imc = peso / (altura * altura);
+    let classificacao = "";
 
-console.log("script carregaodo com sucesso!");
+    if (imc < 18.5) {
+        classificacao = "Abaixo do peso";
+    } else if (imc < 25) {
+        classificacao = "Peso normal";
+    } else if (imc < 30) {
+        classificacao = "Sobrepeso";
+    } else if (imc < 35) {
+        classificacao = "Obesidade Grau I";
+    } else if (imc < 40) {
+        classificacao = "Obesidade Grau II";
+    } else {
+        classificacao = "Obesidade Grau III";
+    }
+
+    document.getElementById("resultado").innerHTML =
+        `Seu IMC é <strong>${imc.toFixed(2)}</strong><br>Classificação: <strong>${classificacao}</strong>`;
+}
+
+console.log("script carregado com sucesso!");
